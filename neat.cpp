8f9cf7a5f542e2925b467b77651a964978dbd7be
Devon O'Connor <devonjoconnor@gmail.com>
2019-03-06 23:21:14 -0500

    Got rid of error(); checking if fin.open() fails

--- neat.cpp
+++ neat.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <stdlib.h>
+#include <cstdlib>
 using namespace std;
-
-void error(string str){
-	cout << str << endl;;
-	exit(1);
-}
-
 
 int main(int argc, char** argv) {
 	ifstream fin;
 
 	/* Check that a file name has been passed */
 	if(argc != 2){
-		error(string("Usage: ") + argv[0] + " <file>");
+		cerr << "Usage: " << argv[0] << " <file>" << endl;;
+		exit(1);
 	} else {
 		/* Open the file for reading */
 		fin.open(argv[1]);
+		if( fin.fail() ){
+			cerr << "Could not open " << argv[1] << endl;
+			exit(1);
+		}
 	}
 
 	vector<int> T;
